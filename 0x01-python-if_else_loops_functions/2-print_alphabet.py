66a26bcdf228b80450d2faaab49403891ae99cc2
LINUSOBURA <linusobura@gmail.com>
2023-11-28 18:27:58 +0300

    lower alphabets

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -3,4 +3,4 @@ asci_values = [97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107,
                108, 109, 110, 111, 112, 113, 114, 115, 116,
                117, 118, 119, 120, 121, 122]
 for asci_value in asci_values:
-    print(f"{chr(asci_value)}", end="")
+    print("{}".format(chr(asci_value)), end="")
