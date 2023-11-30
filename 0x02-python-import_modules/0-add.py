68df27c8901c0f5255ab64c5b5eea0af2667ec6f
LINUSOBURA <linusobura@gmail.com>
2023-11-30 16:28:56 +0300

    import add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
-import add_0 as ad0
+from add_0 import add
 
 a = 1
 b = 2
 
-print("{} + {} = {}".format(a, b, ad0.add(a, b)))
+print("{} + {} = {}".format(a, b, add(a, b)))
+
+if __name__ == "__main__":
+    pass
