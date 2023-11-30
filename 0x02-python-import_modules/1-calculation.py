f05f0d66d64b70c3711988cebe65fc62a784f304
LINUSOBURA <linusobura@gmail.com>
2023-11-30 17:09:15 +0300

    calculator

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
     pass
 
 print("{} + {} = {}".format(a, b, add(a, b)))
-print("{} + {} = {}".format(a, b, sub(a, b)))
-print("{} + {} = {}".format(a, b, mul(a, b)))
-print("{} + {} = {}".format(a, b, div(a, b)))
+print("{} - {} = {}".format(a, b, sub(a, b)))
+print("{} * {} = {}".format(a, b, mul(a, b)))
+print("{} / {} = {}".format(a, b, div(a, b)))
