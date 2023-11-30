6e5e4a7561cf905bf8b174aeef77e7c85854e3fb
LINUSOBURA <linusobura@gmail.com>
2023-11-30 18:35:45 +0300

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
+import sys
+
 if __name__ == "__main__":
     pass
-
-import sys
 
 argv = sys.argv
 
 number_of_args = len(argv)
 
 if number_of_args == 1:
-        print("0 arguments.")
+    print("0 arguments.")
 else:
     if number_of_args == 2:
         print("{} argument:".format(number_of_args-1))
     else:
         print("{} arguments:".format(number_of_args-1))
-    for n in range (1, number_of_args):
+    for n in range(1, number_of_args):
         print("{}: {}".format(n, argv[n]))
