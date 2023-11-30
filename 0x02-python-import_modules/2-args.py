a7c3554a09cb2a19c8b12e0d04865ce8cc02059b
LINUSOBURA <linusobura@gmail.com>
2023-11-30 18:30:18 +0300

    args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import sys
+
+argv = sys.argv
+
+number_of_args = len(argv)
+
+if number_of_args == 1:
+        print("0 arguments.")
+else:
+    if number_of_args == 2:
+        print("{} argument:".format(number_of_args-1))
+    else:
+        print("{} arguments:".format(number_of_args-1))
+    for n in range (1, number_of_args):
+        print("{}: {}".format(n, argv[n]))
