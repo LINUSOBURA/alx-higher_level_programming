927ab824f9d63d7d16d586a64be511d9bc4d9b63
LINUSOBURA <linusobura@gmail.com>
2023-12-05 00:39:41 +0300

    max value

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@
 
 def max_integer(my_list=[]):
     if my_list:
-        max_val = 0
+        max_val = my_list[0]
         for i in my_list:
             if i > max_val:
                 max_val = i
