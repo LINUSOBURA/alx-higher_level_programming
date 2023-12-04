c421dc759ce29d6bee05ee55364bd7397f0fc105
LINUSOBURA <linusobura@gmail.com>
2023-12-04 22:03:29 +0300

    modified

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,9 +3,11 @@
 
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
+        l = 1
         for x in i:
-            if x == i[-1]:
-                print ("{:d}$".format(x), end="")
+            if x == len(i):
+                print ("{:d}".format(x), end="")
             else:
-                print ("{:d} ".format(x), end="")
+                print ("{:d}".format(x), end=" ")
+            l += 1
         print()
