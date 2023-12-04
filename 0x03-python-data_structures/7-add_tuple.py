39ed2956b629f86aa71f638cf64c8384f73a5a80
LINUSOBURA <linusobura@gmail.com>
2023-12-05 00:12:52 +0300

    add tuples

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    len_a = len(tuple_a)
+    len_b = len(tuple_b)
+
+    if len_a < 2:
+        tuple_a += (0,) * (2-len_a)
+    if len_b < 2:
+        tuple_b += (0,) * (2-len_b)
+    new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return new_tuple
