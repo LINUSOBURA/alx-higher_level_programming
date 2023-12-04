b05a6b56ac61dc209cc612c3fca6b0aa99680ddd
LINUSOBURA <linusobura@gmail.com>
2023-12-04 18:42:09 +0300

    return correction

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
 
 def replace_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
-        return(my_list)
+        return (my_list)
     else:
         my_list[idx] = element
-        return(my_list)
+        return (my_list)
