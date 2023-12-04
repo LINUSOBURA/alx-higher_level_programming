3e0459cfde3c519579deb0a5c07ad185afbd6d8b
LINUSOBURA <linusobura@gmail.com>
2023-12-04 15:41:32 +0300

    element at index

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@
 
 def element_at(my_list, idx):
     len_of_list = len(my_list)
-    if idx < 0 or idx >= len_of_list:
+    if idx < 0 or idx > len_of_list - 1:
         return (None)
     else:
         return(my_list[idx])
