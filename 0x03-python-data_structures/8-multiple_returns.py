2ee58c0ba4e1e2af6c6d4d57adafd23add4afd8c
LINUSOBURA <linusobura@gmail.com>
2023-12-05 00:25:26 +0300

    more returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,8 +2,5 @@
 
 
 def multiple_returns(sentence):
-    if not sentence:
-        sentence[0] = None
-    len_sent = len(sentence)
 
-    return (len_sent, sentence[0])
+    return (len(sentence), sentence[0] if sentence else None)
