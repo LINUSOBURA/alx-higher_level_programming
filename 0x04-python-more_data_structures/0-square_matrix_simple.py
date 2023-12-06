1a4bf4e1f8a140f01e83d4fa54a23c7cd7badabc
LINUSOBURA <linusobura@gmail.com>
2023-12-06 15:28:29 +0300

    square matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+
+
+def square_matrix_simple(matrix=[]):
+    new_matrix = [row[:] for row in matrix]
+    for i in range(len(new_matrix)):
+        new_list = []
+        for value in new_matrix[i]:
+            square = value * value
+            new_list.append(square)
+        new_matrix[i] = new_list
+    return new_matrix
