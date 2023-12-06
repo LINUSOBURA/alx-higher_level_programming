896d2abcc43ec366602f8436b0080939d568b4d8
LINUSOBURA <linusobura@gmail.com>
2023-12-06 20:48:39 +0300

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+
+def best_score(a_dictionary):
+    max_value = 0
+    if a_dictionary:
+        for key in a_dictionary:
+            if a_dictionary[key] > max_value:
+                max_value = a_dictionary[key]
+                maximum = key
+        return maximum
+    else:
+        return None
