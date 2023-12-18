248ebf96177d91cd5fcac5220dd626fc440e5c55
LINUSOBURA <linusobura@gmail.com>
2023-12-18 21:36:43 +0300

    err

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except Exception as e:
+        print("Exception: {}".format(str(e)), file=sys.stderr)
+        return False
+
+    else:
+        return True
