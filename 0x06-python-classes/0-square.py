ac76bf4c05d9534c26410c1b44b2a59363b1ec08
LINUSOBURA <linusobura@gmail.com>
2023-12-19 22:05:40 +0300

    initial commit

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""
+
+
+class Square(object):
+    """
     A class representing a square.
 
     Attributes:
@@ -10,11 +13,8 @@
 
     Example:
     square_instance = Square()
-    print(square_instance.side_length)  # Access the side length of the square.
-"""
+    print(square_instance.side_length)  # Access the side length of the square."""
 
-
-class Square(object):
     def __init__(self):
         """
         Initializes a new square instance with a default side length.
