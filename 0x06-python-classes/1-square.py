c2f719ca4dfbb1dd2975ccfc23a5c70ff047b015
LINUSOBURA <linusobura@gmail.com>
2023-12-19 22:22:13 +0300

    square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Square class for representing a square"""
+
+
+class Square:
+    def __init__(self, size):
+        """innit for automatic size creation"""
+        self.__size = size
+        pass
