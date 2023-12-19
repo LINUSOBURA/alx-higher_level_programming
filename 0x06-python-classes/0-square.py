7fd1dccfad641eff754f2dacc9ffb2ecedcda90b
LINUSOBURA <linusobura@gmail.com>
2023-12-19 22:10:36 +0300

    initial commit

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-"""
-    A class representing a square.
-"""
+"""A class representing a square."""
 
 
-class Square(object):
-    def __init__(self):
-        """
-        Initializes a new square instance with a default side length
-        """
-        pass
+class Square:
+    """Initializes a new square instance with a default side length"""
+
+    pass
