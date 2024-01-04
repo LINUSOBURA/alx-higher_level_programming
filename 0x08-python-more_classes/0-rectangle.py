10f7c6f342514fdf16870967e579101c024793ab
LINUSOBURA <linusobura@gmail.com>
2024-01-04 16:25:10 +0300

    simple rectangle class

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 
-""" A simple rectangle class with no functionality"""
+""" A simple rectangle class"""
 class Rectangle():
+    """Pass as there is no functionality"""
     pass
