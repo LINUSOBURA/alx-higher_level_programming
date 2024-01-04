2522aff72d91a4c9e575ca63daccf26850b54568
LINUSOBURA <linusobura@gmail.com>
2024-01-04 17:45:21 +0300

    definition of rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -23,9 +23,9 @@ class Rectangle():
     def height(self, value):
         """setter to modify private attribute height"""
         if type(value) != int:
-            raise TypeError("width must be an integer")
+            raise TypeError("height must be an integer")
         elif value < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("height must be >= 0")
         else:
             self.__height = value
 
