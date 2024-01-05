1662bd19ee35cc5ba49c8fdfe33ec8cd17f8e0dd
LINUSOBURA <linusobura@gmail.com>
2024-01-06 00:07:43 +0300

    Locked class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""A locked class with no attributes"""
+
+
 class LockedClass:
+    """Locked class prevents dynamic creation of instance attributes"""
+
     __slots__ = ("first_name",)
 
     def __init__(self):
