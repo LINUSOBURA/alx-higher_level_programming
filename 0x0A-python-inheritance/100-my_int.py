2d8beaec8d8f22232aa420eeca6e45e04914f828
LINUSOBURA <linusobura@gmail.com>
2024-01-09 01:29:28 +0300

    invert

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,14 @@
+#!/use/bin/python3
+"""Module that inverts != and =="""
+
+
+class MyInt(int):
+    """MyInt Class"""
+
+    def __eq__(self, other):
+        """Returns !="""
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        """Returns =="""
+        return super().__eq__(other)
