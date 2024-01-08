9559ca6c3dce3ab0a67c041fe4e1cd1a72f1269b
LINUSOBURA <linusobura@gmail.com>
2024-01-09 01:42:33 +0300

    Can I

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""Module to add non existing attribute"""
 
 
 def add_attribute(obj, attr, value):
+    """Adds an attribute if possible"""
     if hasattr(obj, "__dict__") or (
         hasattr(type(obj), "__slots__") and attr in type(obj).__slots__
     ):
