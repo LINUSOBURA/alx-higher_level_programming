c65757687619a6f75d7b5e8dcd530f06a4b40e3f
LINUSOBURA <linusobura@gmail.com>
2024-01-08 18:40:06 +0300

    initial commit

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -3,6 +3,5 @@
 
 
 def lookup(obj):
-    """functions returns a list of
-    available attributes aand methods"""
+    """functions returns a list of available attributes aand methods"""
     return list(dir(obj))
