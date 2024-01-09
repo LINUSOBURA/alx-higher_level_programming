ff8ffba472b13b03a6d184dd7eaf68659eff7189
LINUSOBURA <linusobura@gmail.com>
2024-01-09 18:36:16 +0300

    initial commit

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """Function reads a file and prints to stdout"""
     with open(filename, "r", encoding="utf-8") as f:
         read_data = f.read()
-    print(read_data)
+    print(read_data, end="")
