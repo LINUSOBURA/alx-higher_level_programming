265048dcdf75b9ce8dc5e7572b3f2403189e1e0a
LINUSOBURA <linusobura@gmail.com>
2024-01-09 18:48:15 +0300

    Write to file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""Module for writing to a file"""
 
 
 def write_file(filename="", text=""):
+    """Funtion writes to a file, and return number of characters written"""
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
