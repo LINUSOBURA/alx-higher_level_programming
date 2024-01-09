b238053c8968fa3b534a83f3f4bfabc530dc85bd
LINUSOBURA <linusobura@gmail.com>
2024-01-09 19:09:38 +0300

    To json string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module that returns Json Representation"""
+
+import json
+
+
+def to_json_string(my_obj):
+    """Function that returns an object"""
+    return json.dumps(my_obj)
