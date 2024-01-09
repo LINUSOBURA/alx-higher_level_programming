ea33c84a9fb2e586fa2fe0d298d3f35e5a2b1118
LINUSOBURA <linusobura@gmail.com>
2024-01-10 01:49:59 +0300

    fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,23 +2,18 @@
 """Scrript to Load add and save in file"""
 import sys
 
-if __name__ != "__main__":
-    pass
+if __name__ == "__main__":
+    save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+    load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+    arguments = sys.argv
+    filename = "add_item.json"
 
-arguments = sys.argv
-filename = "add_item.json"
+    try:
+        arg_list = load_from_json_file(filename)
+    except FileNotFoundError:
+        arg_list = []
 
-try:
-    arg_list = load_from_json_file(filename)
-except FileNotFoundError:
-    arg_list = []
+    arg_list.extend(arguments[1:])
 
-arg_list.extend(arguments[1:])
-
-save_to_json_file(arg_list, filename)
-
-if __name__ != "__main__":
-    pass
+    save_to_json_file(arg_list, filename)
