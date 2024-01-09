c1f582b9a0cb9aafae78606883564b3257551c85
Linus Obura <56161418+LINUSOBURA@users.noreply.github.com>
2024-01-10 01:34:34 +0300

    Update 10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -19,4 +19,5 @@ class Student:
                 "age": self.age,
             }
         else:
-            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+            return {attr: getattr(self, attr) for attr in attrs\
+                    if hasattr(self, attr)}
