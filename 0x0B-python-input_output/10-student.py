906f4caf15ad9dad98822e3158ae168a47fb9896
LINUSOBURA <linusobura@gmail.com>
2024-01-10 01:26:35 +0300

    student

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module Student To JSON"""
+
+
+class Student:
+    """Class student"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance."""
+        if attrs is None or not isinstance(attrs, list):
+            return {
+                "first_name": self.first_name,
+                "last_name": self.last_name,
+                "age": self.age,
+            }
+        else:
+            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
