ce81341385d8841bd287659e35e0a2b3f2eac507
LINUSOBURA <linusobura@gmail.com>
2024-01-15 21:05:49 +0300

    fix style

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -91,7 +91,8 @@ class Rectangle(Base):
             print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
-        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
+        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y}\
+            - {self.__width}/{self.__height}"
 
     def update(self, *args, **kwargs):
         """Function updates attributes depending on arguments"""
