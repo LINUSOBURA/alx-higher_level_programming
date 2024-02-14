28b27404c9394f58e2d5a922797439b2af01bfa5
LINUSOBURA <linusobura@gmail.com>
2024-02-14 14:10:10 -0800

    Added scripts for 4-8

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,2 @@
+-- cript that creates the table force_name
+CREATE TABLE IF NOT EXISTS force_name(id int, name VARCHAR(256) NOT NULL);
---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,2 @@
+-- script that creates the table unique_id
+CREATE TABLE IF NOT EXISTS unique_id (id int KEY DEFAULT 1, name VARCHAR(256));
