37033ee8fbadae6ef5630eaa6e875c7f8b5d0011
LINUSOBURA <linusobura@gmail.com>
2024-02-15 05:38:56 -0800

    fixed selected fields

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- cript that lists all the cities of California that can be found in the database hbtn_0d_usa
-SELECT * FROM cities
+SELECT id, name FROM cities
 WHERE state_id = (
 	SELECT id FROM states WHERE name = 'california')
 ORDER BY id ASC;
