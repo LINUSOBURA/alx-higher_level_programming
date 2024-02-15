15d96bcff51ab8a920f3ecf8badb4a49580f7711
LINUSOBURA <linusobura@gmail.com>
2024-02-15 07:55:43 -0800

    Advanced tasks 17-20

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,13 @@
+-- 
+SELECT title
+FROM tv_shows
+WHERE id NOT IN (
+	    SELECT show_id
+	    FROM tv_show_genres
+	    WHERE genre_id = (
+		        SELECT id
+			FROM tv_genres
+			WHERE name = 'Comedy'
+			)
+		)
+ORDER BY title ASC;
