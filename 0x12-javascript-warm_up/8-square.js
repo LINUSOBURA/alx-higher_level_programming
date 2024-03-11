f0c4f6477a9f1bfbe9f2de0985a9f99c36ff2f25
LINUSOBURA <linusobura@gmail.com>
2024-03-11 18:20:29 +0300

    task 2 -task 7 answers

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,18 +1,15 @@
 #!/usr/bin/node
 
-args = process.argv;
+const args = process.argv;
 
 const size = parseInt(args[2]);
-let j = 0, i = 0;
+let j = 0;
 
-if (isNaN(size)){
-	console.log('Missing size');
+if (isNaN(size)) {
+  console.log('Missing size');
 } else {
-	while (j < size)
-	{
-		console.log('X'.repeat(size));
-		j += 1;
-	}
-	
-	
+  while (j < size) {
+    console.log('X'.repeat(size));
+    j += 1;
+  }
 }
