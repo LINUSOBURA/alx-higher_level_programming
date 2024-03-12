32733eeb72705e58327be39276969edbb3cba9fb
LINUSOBURA <linusobura@gmail.com>
2024-03-13 01:10:16 +0300

    Scripts for task 2 - 13

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,12 +4,10 @@ const square = require('./5-square');
 
 module.exports = class Square extends square {
   charPrint (c) {
-    for (let i = 0; i < this.size; i++) {
-      if (c) {
-        console.log('C'.repeat(this.size));
-      } else {
-        console.log('x'.repeat(this.size));
-      }
+    if (c) {
+      for (let i = 0; i < this.size; i++) console.log('C'.repeat(this.size));
+    } else {
+      this.print();
     }
   }
 };
