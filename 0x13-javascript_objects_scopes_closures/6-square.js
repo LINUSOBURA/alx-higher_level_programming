5490f4ee7702bf8de050ad7100d8076a82bbe50d
LINUSOBURA <linusobura@gmail.com>
2024-03-13 10:37:53 +0300

    fix c variable change

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const square = require("./5-square");
+const square = require('./5-square');
 
 module.exports = class Square extends square {
-  charPrint(c) {
+  charPrint (c) {
     if (!(c === undefined)) {
       for (let i = 0; i < this.size; i++) console.log(c.repeat(this.size));
     } else {
