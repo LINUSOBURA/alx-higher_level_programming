eca9311a68d9439255fe89a07abce238a90811f0
LINUSOBURA <linusobura@gmail.com>
2024-03-15 17:46:02 +0300

    Initial comm

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Lists all the states of the database hbtn_0e_0_usa
+"""
+import sys
+
+import MySQLdb
+
+
+def mysqlconnect():
+    """
+    Establiishes connection to the database for querrying
+    """
+
+    db_connection = MySQLdb.connect(user=sys.argv[1],
+                                    passwd=sys.argv[2],
+                                    db=sys.argv[3])
+
+    cursor = db_connection.cursor()
+
+    cursor.execute("SELECT * FROM states;")
+
+    for state in cursor.fetchall():
+        print(state)
+
+    db_connection.close()
+
+
+mysqlconnect()
