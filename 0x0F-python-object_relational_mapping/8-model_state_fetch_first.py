805752ad37f8fbf37977c44d6ef33de4abea0b57
LINUSOBURA <linusobura@gmail.com>
2024-03-16 20:35:29 +0300

    first state

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+script that prints the first State object from the database hbtn_0e_6_usa
+"""
+from sys import argv
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from model_state import Base, State
+
+if __name__ == "__main__":
+    dtabase_url = ('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]))
+
+    engine = create_engine(dtabase_url)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    first_state = session.query(State).first()
+
+    if first_state == []:
+        print("Nothing\n")
+    else:
+        print('{}: {}'.format(first_state.id, first_state.name))
