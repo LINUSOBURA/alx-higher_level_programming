2d1c57e1affa6d8aeb731305a76d84c80632435e
linusobura <linusobura@gmail.com>
2024-03-27 06:57:58 -0700

    add newline

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
 # Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
 curl -s -I "$1" | grep -Fi "Content-Length" | awk -F ': ' '{print $2}'
+
--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
 #Bash script that takes in a URL and displays all HTTP methods the server will accept
 curl -sLI "$1" | grep "Allow" | awk -F ': ' '{print $2}'
+
