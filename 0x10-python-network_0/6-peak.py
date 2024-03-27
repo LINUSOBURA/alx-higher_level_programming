525a727e59e4a2ff2e4b9e8bd79864aca338b6c4
LINUSOBURA <linusobura@gmail.com>
2024-03-27 20:10:56 +0300

    fixed

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,24 +4,8 @@
 
 def find_peak(list_of_integers):
     """Function to find peak"""
-    if not list_of_integers:
-        return None
-
-    if len(list_of_integers) == 1:
-        return list_of_integers[0]
-
-    left = 0
-    right = len(list_of_integers) - 1
-
-    while left < right:
-        midium = (left + right) // 2
-
-        if list_of_integers[midium] > list_of_integers[
-                midium -
-                1] and list_of_integers[midium] > list_of_integers[midium + 1]:
-            return list_of_integers[midium]
-        elif list_of_integers[midium] < list_of_integers[midium + 1]:
-            left = midium + 1
-        else:
-            right = midium - 1
-    return list_of_integers[left]
+    max_i = None
+    for ele in list_of_integers:
+        if max_i is None or max_i < ele:
+            max_i = ele
+    return max_i
