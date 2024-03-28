0f576f2fe081ac8107102f3ac755ce769b913c51
LINUSOBURA <linusobura@gmail.com>
2024-03-28 18:14:22 +0300

    initial commit

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 # Python script that dusplays a particular header
+import sys
 import urllib.request
-from sys import argv
 
 if __name__ == "__main__":
-    url = argv[1]
+    url = sys.argv[1]
 
     with urllib.request.urlopen(url) as response:
         x_id = response.getheader("X-Request-Id")
--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 # sends post request and returns a response
+import sys
 import urllib.parse
 import urllib.request
-from sys import argv
 
 if __name__ == "__main__":
-    url = argv[1]
-    values = {'email': argv[2]}
+    url = sys.argv[1]
+    values = {'email': sys.argv[2]}
 
     data = urllib.parse.urlencode(values)
     data = data.encode('ascii')
