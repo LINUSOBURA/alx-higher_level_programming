b180a4f4089ab4baf0552851a53b4620e16f1048
LINUSOBURA <linusobura@gmail.com>
2024-03-28 23:36:15 +0300

    Testing 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -10,5 +10,5 @@ import requests
 
 if __name__ == "__main__":
     url = "http://0.0.0.0:5000/search_user"
-    response = requests.post(url, params={"q": sys.argv[1]})
+    response = requests.post(url, params={"q": "sys.argv[1]"})
     print(response.text)
