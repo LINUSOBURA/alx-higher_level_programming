fdb5e334ade1a83d78b04499b7b614438ed73c2e
LINUSOBURA <linusobura@gmail.com>
2024-03-28 22:08:19 +0300

    script 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""
+ Python script that fetches https://alx-intranet.hbtn.io/status
+"""
+import requests
+
+if __name__ == "__main__":
+    response = requests.get("https://alx-intranet.hbtn.io/status")
+    body = response.text
+    b_type = type(body)
+    print("""Body response:\n\t- type: {}\n\t- content: {}""".format(
+        b_type, body))
