d09e7f49110c6b7c3f0602a8f4fbaebbe50dc98c
LINUSOBURA <linusobura@gmail.com>
2024-03-28 18:30:24 +0300

    initial commit

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,7 +8,6 @@ with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
     body_type = type(body)
     decoded_body = body.decode("utf-8")
 
-print("""Body response:
-      - type: {}
-      - content: {}
-      - utf8 content: {}""".format(body_type, body, decoded_body))
+print(
+    """Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}""".
+    format(body_type, body, decoded_body))
