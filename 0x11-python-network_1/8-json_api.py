8305e2df12284f309743d236d74ebe0beebf5abd
LINUSOBURA <linusobura@gmail.com>
2024-03-28 23:54:33 +0300

    Testing 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -12,4 +12,11 @@ if __name__ == "__main__":
     url = "http://0.0.0.0:5000/search_user"
     response = requests.post(url, params={"q": sys.argv[1]})
     json_resp = response.json()
+    if json_resp:
+        for item in json_resp["items"]:
+            print("[{}] {}".format({item['id'], item['name']}))
+    elif json_resp == {}:
+        print("No result")
+    else:
+        print("Not a valid JSON")
     print(json_resp)
