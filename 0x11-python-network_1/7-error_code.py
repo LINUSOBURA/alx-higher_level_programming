1a4845af8f9d1220967729cbeb98b42249e425ea
LINUSOBURA <linusobura@gmail.com>
2024-03-28 22:53:49 +0300

    script 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""
+ython script that takes in a URL, sends a request
+to the URL and displays the body of the response
+"""
+import sys
+
+import requests
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    response = requests.get(url)
+    try:
+        print(response.text)
+        response.raise_for_status()
+    except requests.HTTPError as error:
+        print("Error code: {}".format(error))
