fa33ad720574e505692fb8cb306c17f99073cbdc
LINUSOBURA <linusobura@gmail.com>
2024-03-29 00:11:27 +0300

    Testing 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -18,9 +18,7 @@ if __name__ == "__main__":
     try:
         json_resp = response.json()
         if json_resp:
-            for item in json_resp:
-                print(item)
-                print("[{}] {}".format(item[0], item[1]))
+            print("[{}] {}".format({json_resp['id']}, {json_resp['name']}))
         else:
             print("No result")
     except ValueError:
