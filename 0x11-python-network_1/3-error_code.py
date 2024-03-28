94cb88e653ab4d2f4186d18e5c777e9396e109eb
LINUSOBURA <linusobura@gmail.com>
2024-03-28 21:43:23 +0300

    error test

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""
+Python script that takes in a URL, sends a request
+to the URL and displays the body of the response (decoded in utf-8).
+"""
+import sys
+import urllib.error
+import urllib.request
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    try:
+        with urllib.request.urlopen(url) as response:
+            body = (response.read()).decode("utf-8")
+            print(body)
+    except urllib.error.HTTPError as error:
+        print("Error code: {}".format(error.status))
