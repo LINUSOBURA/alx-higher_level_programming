42e342a1fbc49a34628cd8dfe0f48a7b14569c4b
LINUSOBURA <linusobura@gmail.com>
2024-03-28 23:29:48 +0300

    Testing 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""
+Python script that takes in a letter and sends a
+POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter.
+"""
+import sys
+
+import requests
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    try:
+        q = sys.argv[1]
+    except:
+        q = ""
+    response = requests.post(url, params=q)
+    print(response.text)
