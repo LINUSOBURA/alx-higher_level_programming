7c309f90363fac9deb77db1ced815506dc95d04f
LINUSOBURA <linusobura@gmail.com>
2024-04-23 19:16:46 +0300

    right order using promises

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,40 @@
+#!/usr/bin/node
+
+const { rejects } = require("assert");
+const { error } = require("console");
+const { resolve } = require("path");
+const request = require("request");
+
+const movieId = process.argv[2];
+const url = "https://swapi-api.alx-tools.com/api/films/" + movieId;
+
+request(url, (err, response, body) => {
+  if (err) {
+    console.error(err);
+  } else {
+    const data = JSON.parse(body);
+
+    const charactersPromises = data.characters.map((characterUrl) => {
+      return new Promise((resolve, reject) => {
+        request(characterUrl, (err, response, body) => {
+          if (err) {
+            reject(err);
+          } else {
+            const charData = JSON.parse(body);
+            resolve(charData.name);
+          }
+        });
+      });
+    });
+
+    Promise.all(charactersPromises)
+      .then((characters) => {
+        characters.forEach((character) => {
+          console.log(character);
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+});
