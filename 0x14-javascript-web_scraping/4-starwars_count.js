78476f49de13834334350ace8079ff4902a6dd32
LINUSOBURA <linusobura@gmail.com>
2024-04-23 17:27:09 +0300

    star wars count

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = process.argv[2];
+
+request(url, (err, response, body) => {
+  if (err) {
+    console.error(err);
+  } else {
+    const data = JSON.parse(body);
+    const films = data.results;
+
+    let count = 0;
+
+    for (const film of films) {
+      if (
+        film.characters.includes(
+          'https://swapi-api.alx-tools.com/api/people/18/'
+        )
+      ) {
+        count++;
+      }
+    }
+    console.log(count);
+  }
+});
