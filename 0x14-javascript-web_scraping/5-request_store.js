b7d3069cbe144fa4f88c6c0f37b1a94ea6c5c564
LINUSOBURA <linusobura@gmail.com>
2024-04-23 17:46:59 +0300

    Loremipsum

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-const request = require("request");
-const fs = require("fs");
+const request = require('request');
+const fs = require('fs');
 
 const url = process.argv[2];
 const filepath = process.argv[3];
@@ -10,7 +10,7 @@ request(url, (error, response, body) => {
   if (error) {
     console.error(error);
   } else {
-    fs.writeFile(filepath, body, "utf8", (err) => {
+    fs.writeFile(filepath, body, 'utf8', (err) => {
       if (err) throw err;
     });
   }
